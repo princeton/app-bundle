e23f7365fce4fa949cc989bfb9989da5f088587e
Kevin Perry <perry@princeton.edu>
2014-09-20 05:22:46 -0400

    Switch to using int milliseconds for timestamps.

--- Princeton/App/DataModel/DocumentObject.php
+++ Princeton/App/DataModel/DocumentObject.php
@@ -1,54 +1,78 @@
 <?php
-
 namespace Princeton\App\DataModel;
 
 use DateTime;
 
+/**
+ * A generic base class for MongoDB Document classes.
+ *
+ * @var string $id - The document's unique ID.
+ * @var boolean $active - Whether the record is active or not.
+ * @var int $created - Timestamp of when the record was created, in milliseconds.
+ * @var int $lastModified - Timestamp of when the record was last modified, in milliseconds.
+ *
+ * @author Kevin Perry, perry@princeton.edu
+ * @copyright 2014 The Trustees of Princeton University.
+ */
 class DocumentObject implements \JsonSerializable
 {
-	protected $id;
-	protected $active;
-	protected $created;
-	protected $lastModified;
 
-	public function __construct()
-	{
-		$this->created = $this->lastModified = new DateTime();
-		$this->active = true;
-	}
+    protected $id;
 
-	public function id()
-	{
-		return $this->id;
-	}
+    protected $active;
 
-	public function isActive()
-	{
-		return ($this->active === true);
-	}
+    protected $created;
 
-	public function activate()
-	{
-		$this->active = true;
-	}
+    protected $lastModified;
 
-	public function deactivate()
-	{
-		$this->active = false;
-	}
-	
-	public function asArray()
-	{
-		return array(
-			'id' => $this->id,
-			'active' => $this->active,
-			'created' => $this->created,
-			'lastModified' => $this->lastModified,
-		);
-	}
-	
-	public function jsonSerialize()
-	{
-		return $this->asArray();
-	}
+    public function __construct()
+    {
+        $this->created = $this->lastModified = self::currentTimeMillis();
+        $this->active = true;
+    }
+
+    public function id()
+    {
+        return $this->id;
+    }
+
+    public function isActive()
+    {
+        return ($this->active === true);
+    }
+
+    public function activate()
+    {
+        $this->active = true;
+    }
+
+    public function deactivate()
+    {
+        $this->active = false;
+    }
+
+    public function updateTimestamp()
+    {
+        $this->lastModified = self::currentTimeMillis();
+    }
+
+    public function asArray()
+    {
+        return array(
+            'id' => $this->id,
+            'active' => $this->active,
+            'created' => $this->created,
+            'lastModified' => $this->lastModified
+        );
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->asArray();
+    }
+
+    protected static function currentTimeMillis()
+    {
+        return (int) (microtime(true) * 1000);
+    }
 }
