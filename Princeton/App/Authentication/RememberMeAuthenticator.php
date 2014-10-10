6f7e45f17da09f6d757ad8fe8181a4c4f114dd73
Kevin Perry <perry@princeton.edu>
2014-10-10 09:43:03 -0500

    comments

--- Princeton/App/Authentication/RememberMeAuthenticator.php
+++ Princeton/App/Authentication/RememberMeAuthenticator.php
@@ -46,6 +46,18 @@ class RememberMeAuthenticator implements Authenticator
 	private function decodeCookie()
 	{
 		// TODO Implement RememberMeAuthenticator.
+		/*
+		 * Device will need to remember:
+		 * {user:'perry',index:0,device:'My Nexus 10',code:'00deadbeef0000deadbeef00'}
+		 * - device name is for ease of user management; index is into device list.
+		 * Database also holds this info; 'code' is an array - may have 2 values...
+		 * {user:'perry',devices:[{name:'My Nexus 10',code:['00deadbeef0000deadbeef00']}]}
+		 * When a value gets old (12hr?), push a 2nd one onto array (atomic findAndUpdate();
+		 * 10 mins later, pop the old one.  This is in case there are multiple parallel reqs. from client.
+		 * Both codes are valid during the interim.  If we recv. a req. with an invalid code, then invalidate all
+		 * codes for that device and force re-authentication (via CAS). Need a UI page to choose "Remember me",
+		 * and one to manage (i.e. delete) configured devices.
+		 */
 		throw new AuthenticationException('RememberMe authentication not implemented!');
 	}
 }
