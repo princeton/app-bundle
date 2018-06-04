0bb60104b718e5b1bb5ee1dc518474ca0a820082
Kevin Perry <perry@princeton.edu>
2018-06-04 09:01:14 -0400

    Make Cache interface Flushable

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -3,8 +3,9 @@
 namespace Princeton\App\Cache;
 
 use Doctrine\Common\Cache\Cache as Doctrine_Cache;
+use Doctrine\Common\Cache\FlushableCache;
 use Princeton\App\Injection\Injectable;
 
-interface Cache extends Doctrine_Cache, Injectable
+interface Cache extends Doctrine_Cache, FlushableCache, Injectable
 {
 }
